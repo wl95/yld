16e571229922d3ccc47baa09aba0c9d50feff9f5
rdmei <1638829333@qq.com>
2018-09-30 14:59:23 +0800

    haha

--- src/components/publicComponent/index.js
+++ src/components/publicComponent/index.js
@@ -1,4 +1,4 @@
-import React,{Fragment,Component} from 'react';
+import React,{Fragment} from 'react';
 import mapStateToProps from './mapState'
 import mapDispatchToProps from './mapDispatch'
 import { connect } from 'react-redux'
@@ -6,7 +6,7 @@ import Filter from './filter'
 import Table from './table'
 import data from 'utils/datas.js'
 import moment from "moment"
-import { request } from 'utils'
+import { request, CalcDiffTime } from 'utils'
 
 const PublicComponent = ({
     setSearch,
@@ -18,48 +18,12 @@ const PublicComponent = ({
         location,
         onFilterSubmit({filed, dateFormat}){
             let urls = "http://10.136.1.216:9091/v1/ycReport";
-            const {DATE_TYPE,UPDATE_DATE_START,UPDATE_DATE_END,ORGAN_LEVEL} =filed;
-            /* for (let item in filed) {
+            for (let item in filed) {
                 if(filed[item] instanceof Object){
                     filed[item] = filed[item].format(dateFormat === 'YYYY-MM-DD' ? 'YYYYMMDD' : 'YYYYMM')
                 }
-            } */
-             /**
-            * 间隔天数
-            */
-            const Times = (faultDate,completeTime) => {
-                console.log(faultDate,completeTime)
-                let stime = Date.parse(new Date(faultDate));
-                let etime = Date.parse(new Date(completeTime));
-                let usedTime = etime - stime;  //两个时间戳相差的毫秒数
-                let days=Math.floor(usedTime/(24*3600*1000));
-                //计算出小时数
-                let leave1=usedTime%(24*3600*1000);    //计算天数后剩余的毫秒数
-                //计算相差分钟数
-                let leave2=leave1%(3600*1000);        //计算小时数后剩余的毫秒数
-                let time = days+1;
-                return JSON.stringify(time);
             }
-            /**
-             * 间隔月数
-             */
-            const getIntervalMonth = (date1,date2) => {
-                // console.log(date1)
-                // console.log(date2)
-                date1 = date1.split("-");
-                date2 = date2.split("-");
-                //获取年,月数
-                var year1 = parseInt(date1[0]) , 
-                month1 = parseInt(date1[1]) , 
-                year2 = parseInt(date2[0]) , 
-                month2 = parseInt(date2[1]) , 
-                //通过年,月差计算月份差
-                months = (year2 - year1) * 12 + (month2-month1)+1;
-                return JSON.stringify(months);    
-            }
-            
-            let JgTime;
-            DATE_TYPE === "0" ? JSON.stringify(JgTime = Times(UPDATE_DATE_START,UPDATE_DATE_END)) : JSON.stringify(JgTime=getIntervalMonth(moment(UPDATE_DATE_START).format('YYYY-MM'),moment(UPDATE_DATE_END).format('YYYY-MM')))
+            let {DATE_TYPE,UPDATE_DATE_START,UPDATE_DATE_END,ORGAN_LEVEL} =filed;
             let datas = {
                 reportName:data[pubChildren].reportName,
                 offset:1,
@@ -67,10 +31,11 @@ const PublicComponent = ({
                 paramMap:{
                     DATE_TYPE,
                     GROUP_BY:"ORGAN_ID",
-                    UPDATE_DATE_START:UPDATE_DATE_START.format(dateFormat === 'YYYY-MM-DD' ? 'YYYYMMDD' : 'YYYYMM'),
-                    UPDATE_DATE_END:UPDATE_DATE_END.format(dateFormat === 'YYYY-MM-DD' ? 'YYYYMMDD' : 'YYYYMM'),
+                    UPDATE_DATE_START:UPDATE_DATE_START,
+                    UPDATE_DATE_END:UPDATE_DATE_END,
                     ORGAN_LEVEL:ORGAN_LEVEL,
-                    DAY_INTERVAL:JgTime
+                    DAY_INTERVAL:'10'
+                    //DAY_INTERVAL:CalcDiffTime(UPDATE_DATE_START,UPDATE_DATE_END, dateFormat)
                 },
                 orderMap:{property:"period",direction:"DESC"}
             };
